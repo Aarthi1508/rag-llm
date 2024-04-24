46dd4a355ad966289ba8459a20707e99fe8d01ec
aarthi1508 <aarthiaug15@gmail.com>
2024-04-23 19:24:59 -0500

    Basic code added

---
+++ QASystem/ingestion.py
@@ -0,0 +1,18 @@
+
+from haystack import Pipeline
+from haystack.components.writers import DocumentWriter
+from haystack.components.preprocessors import DocumentSplitter
+from haystack.components.embedders import SentenceTransformersDocumentEmbedder
+from haystack_integrations.document_stores.pinecone import PineconeDocumentStore
+from haystack.components.converters import PyPDFToDocument
+from pathlib import Path # type: ignore
+import os
+from dotenv import load_dotenv
+from QASystem.utility import pinecone_config
+
+
+def ingest():
+    pass
+
+if __name__=="__main__":
+    ingest()
